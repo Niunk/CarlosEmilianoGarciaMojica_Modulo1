be19eb7b3f01d65725ad778a8764ad375f6f5e54
Niunk <gerechtigkeit@comunidad.unam.mx>
2021-04-29 21:17:13 -0500

    FEAT: carpteta actividad 9

---
+++ Act_9/Act_9.php
@@ -0,0 +1,34 @@
+<?php
+
+//Variable de columnas
+$col = 5;
+//Variable de filas
+$fil = 5;
+//Variable que en función de la fila, determina el color del primer cuadrado
+$var_fil;
+
+echo "<table border='1'>";
+//Iteración que da las filas
+while ($fil>0)
+{
+  //Condición ternaria donde según la fila par o impar toma el valor de 1 o 0
+  $var_fil=($fil%2===0)?0:1;
+  echo "<tr>";
+  //Ciclo para las columnas
+  for($i=1;$i<=$col;$i++)
+  {
+    //En función de la columna, hace comparación con el valor de var_fil(fila par o impar) y empieza por blanco o negro
+    if($i%2===$var_fil)
+    {
+      echo "<td><img src='./negro.jpg' width='100' height='100'></td>";
+    }
+    else
+    {
+      echo "<td><img src='./blanco.jpg' width='100' height='100'></td>";
+    }
+  }
+  echo "</tr>";
+  $fil--;
+}
+echo "</table>";
+?>
